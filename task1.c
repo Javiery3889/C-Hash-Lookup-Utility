a788ac68cfd64b9c2456cfdf95d77c3de1ffecc3
Javiery3889 <javiery3889@gmail.com>
2017-12-30 23:01:34 +0800

    Update task1.c

--- task1.c
+++ task1.c
@@ -12,31 +12,30 @@ Last Updated: 23/12/2017
 #include <ctype.h>
 #include <time.h>
 
-
-void starttime(); //declaring starttime function
-void endtime(); //declaring endtime function
-void words(FILE * filename); //declaring word function and passing the argument filename into it
+void starttime();
+void endtime();
+void words(FILE * filename,int maxsize,int minsize);
 int main(int argc, char const *argv[]){
 	
 	starttime(); // calling starttime function
-	char filename[256];
-	int returnvalue;
+
+	char filename[256]; //declare a char array variable to store the value of the wordlist file name in the command line argument
 	strcpy(filename,argv[1]);
-	int num2 = atoi(argv[2]);
-	int num3 = atoi(argv[3]);
+	int minsize = atoi(argv[2]);
+	int maxsize = atoi(argv[3]);
 
 	FILE *file = fopen(filename,"r");
 
 	if(file == NULL){
 		printf("Fatal error! %s not found\n ", argv[1]);
 	}
-	else if (num2 > num3){
+	else if (minsize > maxsize){
 		printf("Usage : ./task1 <wordlist> <min> <max>\n\t<wordlist> : A file path/name in which contains the password dictionary\n\t<min> : An integer value greater than 1.\n\t\tThis value represents the minimum length of the password\n\t<max> : An integer value greater than or equal to <min>.\n\t\t<max> represents the maximum length of the password.\n");
 	}
-	else{ // if filename ==0 means user entered entered "small_wordlist.txt" for the second arguement
-	words(file); //passing argument file into words function
-	
+	else{ //if file is valid
+	words(file,maxsize,minsize); 
 	}
+
 	endtime(); // calling endtime function
 	return 0; // end of the program
 }
@@ -73,7 +72,7 @@ void endtime(){
 word function takes in a file pointer of the wordlist file in the function parameters, this function hashes each word in the wordlist file using 
 the crypt library, each word is hash to MD5 and SHA-512 until it reaches to the end of the file
 */
-void words(FILE * filename){  
+void words(FILE * filename,int maxsize,int minsize){  
 	char * hash_type_1 = "$1$";	 // type 1 implies md5 (number of iteration is only 1000 rounds)
     char * hash_type_2 = "$6$";	 // type 2 implies sha-512 (default value as in yr 2017, number of iteration is minimum 10,000 rounds )
 	char * salt_1 ="$";			  // a simplified 0 length salt.
@@ -91,11 +90,12 @@ void words(FILE * filename){
 	// To read the file line by line from "smallwordlist until the last line. 
 	//A null pointer is returned when EOF is encountered and no more characters can be read
 		int len = strlen(sizeofline); 
+		if(len > minsize && len < maxsize){ //if the length of word is in the range of the min and max value specified in command line
 		//finding the last character 
 		while( len>0 && isspace(sizeofline[len-1])){ // Searches for "\n" (last character in the password)
-			// printf("%d\n",len); // This would print the length of the first password .. Exmaple C\n   (two character)
+			// printf("%d\n",len); This would print the length of the first password .. Examaple C\n   (two character)
 			len--; 
-			// printf("%d\n",len); // This would print the new length of the first password , which is now C "one character"
+			// printf("%d\n",len); This would print the new length of the first password , which is now C "one character"
 			// It then returns to the while loop
 			// Since len-1 is now just C , it does not fulfil the isspace() requirement and therefore exits the while loop
 		}
@@ -108,9 +108,22 @@ void words(FILE * filename){
 		
 		result = crypt(sizeofline,encyption_scheme2);// store the encrypted value into result using sha-512 encryption
 		fprintf(fp,"%s:%s\n",sizeofline,result);// print the result into mytab2411.txt file
-		count ++; // count the numbers of line in small_wordlist.txt
+		count++; // count the numbers of line in small_wordlist.txt
+		}
+		else{
+			continue;
+		}
 	}	
 	printf("Total number of words processed\t=>%d\n",count ); // number of line in small wordlist
 	printf("Total number of generated entries =>%d\n",count*2); // number of encrypted passwords in both sha-512 and md5
 	
 }
+
+/*
+References:
+ starttime and endtime function adapted from: https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
+ strftime function: https://www.tutorialspoint.com/c_standard_library/c_function_strftime.htm
+ isspace function: https://www.tutorialspoint.com/c_standard_library/c_function_isspace.htm
+ crypt library and crypt function adapted from mkpassword.c
+ fgets function used for reading file: https://stackoverflow.com/questions/9434207/reading-from-file-using-fgets
+*/
